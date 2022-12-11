41513d6ec94c2c3891d2655e5ad9873b72a9a73a
Tiffany Chen <tchen556@gmail.com>
2022-12-11 16:47:55 -0500

    edit img works

--- app/src/main/java/com/example/giftme/Activities/EditItemActivity.java
+++ app/src/main/java/com/example/giftme/Activities/EditItemActivity.java
@@ -1,7 +1,13 @@
 package com.example.giftme.Activities;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -9,12 +15,19 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.giftme.Helpers.DataBaseHelper;
 import com.example.giftme.Helpers.Item;
 import com.example.giftme.R;
 
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Objects;
 
 public class EditItemActivity extends AppCompatActivity {
@@ -28,6 +41,9 @@ public class EditItemActivity extends AppCompatActivity {
     Button cancelButton;
     DataBaseHelper dataBaseHelper;
     ImageButton backButton;
+    Bitmap bitmap;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +57,7 @@ public class EditItemActivity extends AppCompatActivity {
         linkET = findViewById(R.id.linkET);
         priceET = findViewById(R.id.itemPriceET);
         ratingBar = findViewById(R.id.ratingBar);
+        context = getApplicationContext();
 
         cancelButton = findViewById(R.id.cancel);
         saveButton = findViewById(R.id.save);
@@ -75,21 +92,67 @@ public class EditItemActivity extends AppCompatActivity {
         linkET.setText(item.getWebsite());
         ratingBar.setRating(itemHearts);
 
+        //choose image
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    Intent imgIntent = result.getData();
+                    if (imgIntent != null) {
+                        Uri image = imgIntent.getData();
+                        imgView.setImageURI(image);
+                        try {
+                            // Could lead to OOM if image too large (10K * 10K)
+                            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image);
+                        } catch (Exception e) {
+                            Toast.makeText(this, "Cannot convert Uri to bitmap", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(EditItemActivity.this,
+                                "No image selected", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        imgView.setOnClickListener(view -> openGallery());
+        //choose image end --------------
+
         saveButton.setOnClickListener(view -> {
-            String newName = String.valueOf(nameET.getText());
-            String newDescription = String.valueOf(descriptionET.getText());
-            int newPrice = Integer.parseInt(String.valueOf(priceET.getText()));
-            int newRating = (int) ratingBar.getRating();
-            String newLink = String.valueOf(linkET.getText());
 
-            dataBaseHelper.updateById(collectionName, newLink, item.getId(), newName, newPrice,
-                    newDescription, newRating, item.getImg(), item.getFireStoreID());
-            Toast.makeText(this, "Updated!", Toast.LENGTH_SHORT).show();
+            try{
+                String date;
+                Date dateObj = new Date();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    date = LocalDate.now().toString();
+                } else {
+                    @SuppressLint("SimpleDateFormat") DateFormat dateFormat
+                            = new SimpleDateFormat("yyyy-MM-dd");
+                    date = dateFormat.format(dateObj);
+                }
 
-            Intent myCollectionItemsIntent = new Intent(this, MyCollectionItems.class);
-            myCollectionItemsIntent.putExtra("collection_name", collectionName);
-            finish();
-            startActivity(myCollectionItemsIntent);
+                String fileName = dateObj.getTime() + ".jpg";
+                FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+                fileOutputStream.close();
+                dataBaseHelper.storeImageFirebase(bitmap, fileName);
+
+                String newName = String.valueOf(nameET.getText());
+                String newDescription = String.valueOf(descriptionET.getText());
+                int newPrice = Integer.parseInt(String.valueOf(priceET.getText()));
+                int newRating = (int) ratingBar.getRating();
+                String newLink = String.valueOf(linkET.getText());
+                String newImg = context.getApplicationContext().getFilesDir() + "/" + fileName;
+
+                dataBaseHelper.updateById(collectionName, newLink, item.getId(), newName, newPrice,
+                        newDescription, newRating, newImg, item.getFireStoreID());
+                Toast.makeText(this, "Updated!", Toast.LENGTH_SHORT).show();
+
+                Intent myCollectionItemsIntent = new Intent(this, MyCollectionItems.class);
+                myCollectionItemsIntent.putExtra("collection_name", collectionName);
+                finish();
+                startActivity(myCollectionItemsIntent);
+            }catch (Exception e){
+                Toast.makeText(EditItemActivity.this, "Save image NOT success", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
         });
 
         cancelButton.setOnClickListener(view -> {
@@ -100,4 +163,10 @@ public class EditItemActivity extends AppCompatActivity {
             startActivity(myCollectionItemsIntent);
         });
     }
+
+    private void openGallery() {
+        Intent imgIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        activityResultLauncher.launch(imgIntent);
+    }
+
 }
