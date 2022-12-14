118eb21d8db5858814dbeabda6117411f7cef03c
Jinpeng Lyu <Louis201307@outlook.com>
2022-12-13 20:20:47 -0500

    Add push notification

--- app/src/main/java/com/example/giftme/Helpers/PushNotificationService.java
+++ app/src/main/java/com/example/giftme/Helpers/PushNotificationService.java
@@ -1,7 +1,13 @@
 package com.example.giftme.Helpers;
 
-import androidx.annotation.NonNull;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.giftme.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -14,7 +20,23 @@ public class PushNotificationService extends FirebaseMessagingService {
         String title = Objects.requireNonNull(message.getNotification()).getTitle();
         String body = message.getNotification().getBody();
         String CHANNEL_ID = "MESSAGE";
-
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Message Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            getSystemService(NotificationManager.class).createNotificationChannel(channel);
+            Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setAutoCancel(true);
+            NotificationManagerCompat.from(this).notify(1, notification.build());
+        }
         super.onMessageReceived(message);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(getApplicationContext());
+        dataBaseHelper.setDeviceMessagingToken(SessionManager.getUserEmail(getApplicationContext()));
+        super.onNewToken(token);
     }
 }
